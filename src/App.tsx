1c96351e804e06403601010703bf90e6ab1b9304
Lan Xia <lanxiawork@gmail.com>
2026-01-07 22:39:47 -0500

    add filters
    Signed-off-by: Lan Xia <lanxiawork@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,64 @@
-import { Button, Input, Checkbox } from 'antd';
-import { DeleteTwoTone } from '@ant-design/icons';
-import "./todo.css"
+import { Button, Input, Checkbox } from "antd";
+import { DeleteTwoTone } from "@ant-design/icons";
+import "./todo.css";
+import { useState } from "react";
 
+interface Task {
+  title: string;
+  isCompleted?: boolean;
+}
 function App() {
+  const [tasks, setTask] = useState<Task[]>([{
+    title: 'jp is awesome'
+  }]);
+
+  const [input, setInput] = useState("");
+  const [filter, setFilter] = useState("All");
+  const filteredTasks = tasks.filter(t => {
+    if (filter === "In Progress") {
+      return !t.isCompleted;
+    } else if (filter === "Completed") {
+      return t.isCompleted;
+    } else {
+      return true;
+    }
+  });
 
   return (
     <>
-      <div className='main'>
+      <div className="main">
         <h1>Task Master</h1>
-        <div className='task-creator'>
-          <Input placeholder="Task to be added" />
-          <Button type="primary">Add</Button>
+        <form className="task-creator" onSubmit={(e) => {
+          e.preventDefault();
+          setTask([...tasks, { title: input, isCompleted: false }]);
+          setInput("");
+        }}>
+          <Input placeholder="Task to be added" value={input} onChange={e => { setInput(e.target.value) }} />
+          <Button type="primary" htmlType="submit" disabled={!input}>Add</Button>
+        </form>
+        <div className="task-filter">
+          <Button type={filter === "All" ? "primary" : "default"} onClick={() => { setFilter("All") }} >All</Button>
+          <Button type={filter === "In Progress" ? "primary" : "default"} onClick={() => { setFilter("In Progress") }} >In Progress</Button>
+          <Button type={filter === "Completed" ? "primary" : "default"} onClick={() => { setFilter("Completed") }} >Completed</Button>
         </div>
-        <div className='task-filter'>
-          <Button type="primary">All</Button>
-          <Button type="default">In Progress</Button>
-          <Button type="default">Completed</Button>
-        </div>
-        {['1', '2', '3'].map(item => {
-          return <div className='task'>
-            <Checkbox>{item}</Checkbox>
-            <Button type="default" shape='circle' icon={<DeleteTwoTone />} />
-
-          </div>
+        {filteredTasks.map((item) => {
+          return (
+            <div className="task">
+              <Checkbox checked={item.isCompleted} onChange={(e) => {
+                setTask(tasks.map(t => {
+                  if (t === item) {
+                    t.isCompleted = e.target.checked;
+                  }
+                  return t;
+                }));
+              }}>{item.title}</Checkbox>
+              <Button type="default" shape="circle" icon={<DeleteTwoTone />} onClick={() => {
+                setTask(tasks.filter(t => t !== item))
+              }} />
+            </div>
+          );
         })}
-        <div className='stats'>
-          Total | In Progress | Completed
-        </div>
+        <div className="stats">Total: {tasks.length} | In Progress:  {tasks.filter(t => !t.isCompleted).length} | Completed: {tasks.filter(t => t.isCompleted).length}</div>
       </div>
     </>
   );
